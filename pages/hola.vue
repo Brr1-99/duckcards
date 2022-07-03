18689da673a0bb8536dea09a98decdba8d35bed3
David M. Fajardo <davidmf99@gmail.com>
2022-07-03 14:15:54 +0200

    nuxt 3 base

---
+++ pages/hola.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-zinc-900 text-white">
+
+<h1>hola inside pages
+
+    
+</h1>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-zinc-900 text-white">
+
+<h1>index inside pages
+
+    
+</h1>
+</div>
+</template>
